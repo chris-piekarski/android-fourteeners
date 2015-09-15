e1f65c6017c12569dcc2c33017bbf48bc871281a
Chris Piekarski <chris.piekarski@spectralink.com>
2015-09-15 13:38:08 -0600

    add license activity and update to GNU v3

--- app/src/main/java/com/cpiekarski/fourteeners/activities/LicenseActivity.java
+++ app/src/main/java/com/cpiekarski/fourteeners/activities/LicenseActivity.java
@@ -3,11 +3,14 @@ package com.cpiekarski.fourteeners.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.cpiekarski.fourteeners.R;
+
 
 public class LicenseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
     }
 
     @Override
