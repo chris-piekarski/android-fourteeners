5f6c99e3bec58b2324d4b8fd7b946aa1a783538f
Chris Piekarski <chris.piekarski@spectralink.com>
2015-09-14 23:50:41 -0600

    add leave no trace copyright to layout

--- app/src/main/java/com/cpiekarski/fourteeners/activities/LeaveNoTraceActivity.java
+++ app/src/main/java/com/cpiekarski/fourteeners/activities/LeaveNoTraceActivity.java
@@ -3,11 +3,14 @@ package com.cpiekarski.fourteeners.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.cpiekarski.fourteeners.R;
+
 
 public class LeaveNoTraceActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_no_trace);
     }
 
     @Override
